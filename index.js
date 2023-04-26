646e04fc66343f18e25659f5aae03d101730da91
sharifbekk <mustafozodashq888@gmail.com>
2023-04-26 14:59:41 +0500

    newww

---
+++ index.js
@@ -0,0 +1,98 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const person = {
+//     name: 'John',
+//     age: 30,
+//     hobbies: ['reading', 'running']
+// };
+
+// console.log(person.name); // output: 'John'
+// console.log(person['age']); // output: 30
+
+
+
+
+// const person = {
+//     name: 'John',
+//     age: 30,
+//     hobbies: ['reading', 'running']
+// };
+
+
+
+
+// function Person(name, age, hobbies) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+// }
+
+// const john = new Person('John', 30, ['reading', 'running']);
+
+
+
+
+
+
+// const person = {
+//     name: 'John',
+//     age: 30,
+//     hobbies: ['reading', 'running']
+// };
+
+// const entries = Object.entries(person);
+// console.log(entries); // output: [['name', 'John'], ['age', 30], ['hobbies', ['reading', 'running']]]
+
+
+
+
+// const person = {
+//     name: 'John',
+//     age: 30,
+//     hobbies: ['reading', 'running']
+// };
+
+// const keys = Object.keys(person);
+// console.log(keys); // output: ['name', 'age', 'hobbies']
+
+
+
+
+const person = {
+    name: 'John',
+    age: 30,
+    hobbies: ['reading', 'running']
+};
+
+const values = Object.values(person);
+console.log(values); // output: ['John', 30, ['reading', 'running']]
+
+
+
+
+
+
+
